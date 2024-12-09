f8f39e44e0c19e19ed838d85bb189d8d64d47909
Michael Hu <mbh326684@gmail.com>
2024-12-08 23:41:10 -0500

    implemented most tables and created rudimentary section lister

--- rutgers_schedule_planner/course_list/models.py
+++ rutgers_schedule_planner/course_list/models.py
@@ -6,6 +6,9 @@ class School(models.Model):
     code = models.IntegerField()
     title = models.CharField(max_length=255)
 
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=["code"], name="unique_school_code")]
+
     def __str__(self):
         return self.title
 
@@ -14,14 +17,17 @@ class Subject(models.Model):
     code = models.IntegerField()
     title = models.CharField(max_length=255)
 
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=["code"], name="unique_subject_code")]
+
     def __str__(self):
         return self.title
 
 
 class Course(models.Model):
-    school_code = models.ForeignKey(School, on_delete=models.CASCADE)
-    subject_code = models.ForeignKey(Subject, on_delete=models.CASCADE)
-    course_code = models.IntegerField()
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    code = models.IntegerField()
     title = models.CharField(max_length=255)
 
     def __str___(self):
@@ -30,9 +36,13 @@ class Course(models.Model):
 
 class Section(models.Model):
     index = models.IntegerField()
+    number = models.CharField(max_length=255)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     instructor = models.CharField(max_length=255)
-    is_open = models.BooleanField()
+    open_status = models.BooleanField()
+
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=["index"], name="unique_section_index")]
 
 
 class SectionClass(models.Model):
@@ -40,3 +50,6 @@ class SectionClass(models.Model):
     time = models.TimeField()
     campus = models.CharField(max_length=255)
     building = models.CharField(max_length=255)
+
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=["time"], name="unique_section_class_time")]
--- rutgers_schedule_planner/course_list/views.py
+++ rutgers_schedule_planner/course_list/views.py
@@ -4,8 +4,9 @@ import json
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.db import transaction
 
-from .models import Course
+from .models import School, Subject, Course, Section, SectionClass
 
 
 def index(request):
@@ -20,20 +21,52 @@ def update_courses(request):
     response = requests.post("https://classes.rutgers.edu/soc/api/courses.json", data=params)
     data = json.loads(response.text)
 
-    courses = []
-    for course in data:
-        fields = {
-            "school_code": course["school"]["code"],
-            "subject_code": course["subject"],
-            "course_code": course["courseNumber"],
-            "title": course["title"],
-        }
-        courses.append(Course(**fields))
+    with transaction.atomic():
+        School.objects.all().delete()
+        Subject.objects.all().delete()
+        Course.objects.all().delete()
 
-    Course.objects.bulk_create(courses)
+        courses = []
+        sections = []
+        for course_data in data:
+            school_fields = {
+                "code": course_data["school"]["code"],
+                "title": course_data["school"]["description"],
+            }
+            school, _ = School.objects.get_or_create(**school_fields)
+
+            subject_fields = {
+                "code": course_data["subject"],
+                "title": course_data["subjectDescription"],
+            }
+            subject, _ = Subject.objects.get_or_create(**subject_fields)
+
+            course_fields = {
+                "school": school,
+                "subject": subject,
+                "code": course_data["courseNumber"],
+                "title": course_data["title"],
+            }
+            course = Course(**course_fields)
+            courses.append(course)
+
+            for section_data in course_data["sections"]:
+                section_fields = {
+                    "index": section_data["index"],
+                    "number": section_data["number"],
+                    "course": course,
+                    "instructor": section_data["instructorsText"],
+                    "open_status": section_data["openStatus"],
+                }
+                section = Section(**section_fields)
+                sections.append(section)
+
+        Course.objects.bulk_create(courses)
+        Section.objects.bulk_create(sections)
+
     return HttpResponseRedirect(reverse("course_list:course_selection"))
 
 
 def course_selection(request):
-    courses = Course.objects.all()
+    courses = Course.objects.all().order_by("school", "subject", "code")
     return render(request, "course_list/course_selection.html", {"courses": courses})
