f6b737108ef57928c8247617669e33156b3ee3ec
Michael Hu <mbh326684@gmail.com>
2024-12-15 22:18:27 -0500

    jquery for collapsible courses

--- rutgers_schedule_planner/static/course_list/collapsible_course.js
+++ rutgers_schedule_planner/static/course_list/collapsible_course.js
@@ -1,15 +1,6 @@
-var coll = document.getElementsByClassName("collapsible_course");
-
-for (var i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", collapse_course);
-}
-
-function collapse_course() {
-    this.classList.toggle("active_collapsible");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-        content.style.display = "none";
-    } else {
-        content.style.display = "block";
-    }
-}
+$(document).ready(function () {
+    $(".collapsible_course").click(function () {
+        $(this).toggleClass("activate_collapsible")
+        $(this).next().toggle();
+    })
+});
