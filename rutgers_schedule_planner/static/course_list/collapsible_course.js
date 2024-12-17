6adf83e7edcb10743f80cb8a96b62b4bbae10285
Michael Hu <mbh326684@gmail.com>
2024-12-16 21:45:00 -0500

    tables to display course information

--- rutgers_schedule_planner/static/course_list/collapsible_course.js
+++ rutgers_schedule_planner/static/course_list/collapsible_course.js
@@ -1,4 +1,7 @@
-$(".collapsible_course").click(function () {
+$(".collapsible_course").click(function (e) {
+    if($(e.target).is("button")){
+        return;
+    }
     $(this).toggleClass("active_collapsible");
     $(this).next().toggle();
 })
